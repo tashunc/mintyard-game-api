4300836d659fdf14cc952ca46eac5d59c0207612
rook <lilrookmail@gmail.com>
2022-04-08 14:00:26 +0530

    initial commit with graphql

---
+++ routes/gateway.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', function (req, res, next) {
+     res.send('index');
+});
+
+router.get('/a', function (req, res, next) {
+    res.send( {title: 'Express'});
+});
+module.exports = router;
