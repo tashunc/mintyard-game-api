e6bcb9c7bcdf9d8b290d727d0e74c79056cf046d
lilrook <lilrook@gmail.com>
2022-04-18 11:04:52 +0530

    add contract ID and more logs to error handle

--- src/routes/gateway.ts
+++ src/routes/gateway.ts
@@ -1,21 +1,20 @@
 import express from 'express'
 import Score from '../models/Score'
-import {throws} from "assert";
 
 export const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    console.log('Hit for /')
-    res.send('index');
+    res.send('API Backend is up and Running!!');
 });
 
 
 router.post('/addScore', function (request, response) {
     console.log('Hit for /addScore')
+    console.log(request);
     const score = new Score({
-        // _id: request.body.walletId + '_' + request.body.nftId,
         walletId: request.body.walletId,
+        contractId: request.body.contractId,
         nftId: request.body.nftId,
         contestId: request.body.contestId,
         turns: request.body.turns,
@@ -25,7 +24,7 @@ router.post('/addScore', function (request, response) {
     if (!score || (!score.contestId || !score.nftId || !score.walletId)) {
         response.send("Please Enter Valid Data");
     }
-    score.save( (err: any)=> {
+    score.save((err: any) => {
         if (err) {
             response.send(err);
         } else {
@@ -33,11 +32,13 @@ router.post('/addScore', function (request, response) {
         }
     });
 });
-// update
-// wallet -> nft -> attempts
-router.get('/getScores', (request, response) =>{
+/**
+ * get scores for all contests
+ */
+router.get('/getScores', (request, response) => {
     console.log('Hit for /getScores')
-    Score.find( (err, scores) => {
+    console.log(request);
+    Score.find((err, scores) => {
         if (!err) {
             response.send(scores);
         } else {
@@ -45,29 +46,66 @@ router.get('/getScores', (request, response) =>{
         }
     });
 });
-
-router.post('/updateScores', (request, response) => {
-    const score = new Score({
-        // _id: request.body.walletId + '_' + request.body.nftId,
-        walletId: request.body.walletId,
-        nftId: request.body.nftId,
-        contestId: request.body.contestId,
-        turns: request.body.turns,
-        time: request.body.time,
-
+/**
+ * get scores for a selected contest and contract ID
+ */
+router.get('/getScoresForContestId', (request, response) => {
+    console.log('Hit for /getScoresForContestId')
+    console.log(request);
+    Score.find({
+        contractId: request.body.contractId,
+        contestId: request.body.contestId
+    }, (err, scores) => {
+        if (!err) {
+            response.send(scores);
+        } else {
+            response.send(err)
+        }
     });
+});
+/**
+ * get scores for all contests and contract ID
+ */
+router.get('/getAllNftId', (request, response) => {
+    console.log('Hit for /getAllNftId')
+    console.log(request);
+    Score.find({
+        contractId: request.body.contractId,
+        contestId: request.body.contestId
+    }, (err, scores) => {
+        const nftIds: any[] = [];
+        scores.forEach(score => {
+            nftIds.push(score.nftId)
+        })
+        if (!err) {
+            response.send(nftIds);
+        } else {
+            response.send(err)
+        }
+    });
+});
 
-    Score.updateOne({nftId: request.body.nftId, contestId: request.body.contestId}, {
+router.put('/updateScores', (request, response) => {
+    console.log('Hit for /updateScores')
+    console.log(request);
+    Score.updateOne({
+        walletId: request.body.walletId,
+        contractId: request.body.contractId,
+        nftId: request.body.nftId,
+        contestId: request.body.contestId
+    }, {
         $set: {
-            walletId: request.body.walletId,
             turns: request.body.turns,
             time: request.body.time,
         }
-    }).then((result:any) => {
+    }).then((result: any) => {
         if (result) {
             console.log(result);
         }
-        response.status(200).json({ message: "Update successful! " + result });
+        response.status(200).json({message: "Update successful! " + result});
+    }).catch((error: any) => {
+        response.status(500).json({message: "Update unSuccessful please contact support with a Screen Shot : " + error});
+
     });
 });
 
